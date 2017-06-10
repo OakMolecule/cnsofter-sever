31e1980a53c182bdd256f2fe3cba2342d042aac0
oak <15028690629@139.com>
2017-06-10 23:21:12 +0800

    完善定时功能

--- main.go
+++ main.go
@@ -16,29 +16,69 @@ import (
 var httpport = ":9999"
 
 type medicine struct {
-	Amount       uint
-	Etime        string
-	ID           uint
-	MedicineName string
-	Stime        string
-	Times        uint
+	Amount       uint   //每次服用的片数
+	Etime        string //结束服用时间
+	ID           uint   //ID号
+	MedicineName string //药品名
+	Stime        string //开始服用时间
+	Times        uint   //次数
 	BaseObjID    int16
+}
+
+type remind struct {
+	MedicineName string //药品名称
+	Times        uint   //每天服用次数
+	Amount       uint   //每次服用片数
 }
 
 var db *sql.DB
 
-func remind() {
+func remindTime() {
+	var remindStruct []remind
+	i := 0
+	rows, err := db.Query("SELECT medicine_name,times,amount FROM remind WHERE s_time<=DATE(NOW())")
+	checkErr(err)
+
+	// Get column names
+	columns, err := rows.Columns()
+	if err != nil {
+		panic(err.Error()) // proper error handling instead of panic in your app
+	}
+
+	// Make a slice for the values
+	values := make([]sql.RawBytes, len(columns))
+	fmt.Println(len(values))
+
+	// rows.Scan wants '[]interface{}' as an argument, so we must copy the
+	// references into such a slice
+	// See http://code.google.com/p/go-wiki/wiki/InterfaceSlice for details
+	scanArgs := make([]interface{}, len(values))
+	for i := range values {
+		scanArgs[i] = &values[i]
+	}
+
+	for rows.Next() {
+		fmt.Println("fadfasdfasdfsa")
+		err = rows.Scan(&remindStruct[i].MedicineName, &remindStruct[i].Times, &remindStruct[i].Amount)
+		checkErr(err)
+		fmt.Println(remindStruct[i].MedicineName)
+		i++
+	}
+	remindJson, err := json.Marshal(remindStruct)
+	remindJsonIndent, err := json.MarshalIndent(remindStruct, "", "     ")
+	fmt.Println(remindJson)
+	fmt.Println(remindJsonIndent)
 }
 
 func checkMedicine() {
-	_, err := db.Prepare("DELETE * FROM remind WHERE e_time<DATE(NOW())")
+	stmt, err := db.Prepare("DELETE * FROM remind WHERE e_time<DATE(NOW())")
+	_, err = stmt.Exec()
 	checkErr(err)
 }
 
 func sayhelloName(w http.ResponseWriter, r *http.Request) {
 	var med []medicine
 	r.ParseForm()
-	fmt.Println(r.Form)
 	fmt.Println(r.Host)
 	b, err := ioutil.ReadAll(r.Body)
 	if err != nil {
@@ -69,17 +109,26 @@ func sayhelloName(w http.ResponseWriter, r *http.Request) {
 func main() {
 	var err error
 
-	c := cron.New()
-	specCheckMedicine := "0 0 0 1/1 * * *" //设定每天早上检查药品是否过期
-	specRemind := "0 0 7,12,19 * * *"      //设定每天提醒时间
-
-	c.AddFunc(specCheckMedicine, checkMedicine)
-	c.AddFunc(specRemind, remind)
-	c.Start()
-
 	//链接数据库
 	db, err = sql.Open("mysql", "root:root@tcp(localhost:3306)/cnsoftbei?charset=utf8")
 	checkErr(err)
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		panic(err.Error()) // proper error handling instead of panic in your app
+	}
+	remindTime()
+
+	c := cron.New()
+	specCheckMedicine := "0 0 0 0/1 * *" //设定每天早上检查药品是否过期
+	//specRemind := "0 0 7,12,19 * * *"      //设定每天提醒时间
+	specRemind := "0 */1 * * * *" //设定每天提醒时间
+
+	c.AddFunc(specCheckMedicine, checkMedicine)
+	c.AddFunc(specRemind, remindTime)
+	c.Start()
+	fmt.Println("ffffffffffffffffffffffffffffffffffff")
 
 	//开始http监听
 	http.HandleFunc("/", sayhelloName)
@@ -91,6 +140,7 @@ func main() {
 
 func checkErr(err error) {
 	if err != nil {
+		log.Fatal("", err)
 		panic(err)
 	}
 }
